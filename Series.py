85cd58930b494d09f45a6a58cdcd4ab32c903bcc
randy payano <randypayano@gmail.com>
2022-08-30 16:19:12 -0400

    Series Class done

--- Series.py
+++ Series.py
@@ -13,37 +13,24 @@ class Series(Fed):
         url = self.url_base 
         return self.get(url, {"series_id": self.series_id})
     
-    
-    def categories(self):
-        """Get the categories for an economic data series.
-        Returns:
-            _type_: _description_
-        """
-
-        url = self.url_base + "/categories"
-        return self.get(url, {"series_id": self.series_id})
-
     def observations(self):
-        """Get the observations or data values for an economic data series.
-
+        """fred/series/observations - Get the observations or data values for an economic data series.
         Returns:
             _type_: _description_
         """
         url = self.url_base + "/observations"
         return self.get(url, {"series_id": self.series_id})
-
+    
     def release(self):
-        """Get the release for an economic data series.
-
+        """fred/series/release - Get the release for an economic data series.
         Returns:
             _type_: _description_
         """
         url = self.url_base + "/release"
         return self.get(url, {"series_id": self.series_id})
-
+    
     def search(self):
-        """Get economic data series that match keywords.
-
+        """fred/series/search - Get economic data series that match keywords.
         Returns:
             _type_: _description_
         """
@@ -51,24 +38,46 @@ class Series(Fed):
         return self.get(url, {"series_id": self.series_id})
 
 
-test = Series(
-        apikey = "127d428fd5917c2a35047e76f0b92fd0", 
-        series_id = 'GNPCA'
-    )
+    def search_tags(self):
+        """fred/series/search/tags - Get the tags for a series search.
+        Returns:
+            _type_: _description_
+        """
 
+        url = self.url_base + "/search/tags"
+        return self.get(url, {"series_id": self.series_id})
 
-test
-
-"""
-    Series
+    def search_related_tags(self):
+        """fred/series/search/related_tags - Get the related tags for a series search.
+        Returns:
+            _type_: _description_
+        """
+        
+        url = self.url_base + "/search/related_tags"
+        return self.get(url, {"series_id": self.series_id})
     
-    fred/series/categories - Get the categories for an economic data series.
-    fred/series/observations - Get the observations or data values for an economic data series.
-    fred/series/release - Get the release for an economic data series.
-    fred/series/search - Get economic data series that match keywords.
-    fred/series/search/tags - Get the tags for a series search.
-    fred/series/search/related_tags - Get the related tags for a series search.
-    fred/series/tags - Get the tags for an economic data series.
-    fred/series/updates - Get economic data series sorted by when observations were updated on the FRED® server.
-    fred/series/vintagedates - Get the dates in history when a series' data values were revised or new data values were released.
-"""
+    def tags(self):
+        """fred/series/tags - Get the tags for an economic data series.
+        Returns:
+            _type_: _description_
+        """
+
+        url = self.url_base + "/tags"
+        return self.get(url, {"series_id": self.series_id})
+
+    def updates(self):
+        """fred/series/updates - Get economic data series sorted by when observations were updated on the FRED® server.
+        Returns:
+            _type_: _description_
+        """
+
+        url = self.url_base + "/updates"
+        return self.get(url, {"series_id": self.series_id})
+
+    def vintagedates(self):
+        """fred/series/vintagedates - Get the dates in history when a series' data values were revised or new data values were released.
+        Returns:
+            _type_: _description_
+        """
+        url = self.url_base + "/vintagedates"
+        return self.get(url, {"series_id": self.series_id})
